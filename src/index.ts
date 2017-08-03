bd680a31d8b60d2ca4ed13238afe48b9f66ce6bb
molenzwiebel <info@thijsmolendijk.nl>
2017-08-03 11:50:58 +0200

    Empty repository for v2 merge.

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { field, children } from "./decorators";
-import { Based } from "./wrapper";
-import { Basie, B } from "./types";
-import Database from "./database";
-
-export default Based;
-export { field, children, Based, Basie, B, Database };
